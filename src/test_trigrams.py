93eca2abf8a04d416a6078abc2d7480d3e518a33
Julien Wilson <julienawilson@gmail.com>
2016-12-07 14:18:11 -0800

    first test for bigram and moved trigram.py to src/

--- src/test_trigrams.py
+++ src/test_trigrams.py
@@ -4,7 +4,18 @@
 test_string = "I Wish I may I wish I might"
 
 
-def test_tri_is_list():
-    """Test whether tri is a non empty list."""
-    from trigrams import tri
-    assert type(tri) is list and len(tri) is not 0
+test_list = ["sddf", "dfsfs", "sdgsdf", "word", "another", "blue", "orange"]
+
+
+def test_make_list():
+    """Test whether function makes list."""
+    from trigrams import make_baselist, sentence
+    new_list = make_baselist(sentence)
+    assert type(new_list) == list
+
+
+def test_make_bigrams():
+    """Test whether function makes bigram."""
+    from trigrams import make_bigram
+    new_bigram = make_bigram(test_list)
+    assert len(new_bigram[1].split()) == 2
