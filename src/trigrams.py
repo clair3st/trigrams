99d6ca6186d3af4c3de719f5fbe3fb0185cacfc1
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-07 14:35:00 -0800

    added new function make_bigrams

--- src/trigrams.py
+++ src/trigrams.py
@@ -8,3 +8,11 @@ def make_baselist(sentence):
     """Create baselist from an inputted sentence."""
     baselist = sentence.split()
     return baselist
+
+def make_bigram(baselist):
+    """Make bigrams out of an ordinary list."""
+    bigrams = []
+    for c, i in enumerate(baselist, 2):
+            bigrams.append(baselist[c - 2] + " " + baselist[c - 1])
+            if c == len(baselist):
+                return bigrams
