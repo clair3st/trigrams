34d845866a3f8661af37ba2be0cab2b8186cd95b
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-07 14:02:55 -0800

    initial commit for the Trigram Algo program

---
+++ trigrams.py
@@ -0,0 +1,10 @@
+"""Write new stories based on inputted sentences or books using trigram algo."""
+
+
+sentence = 'I wish I may I wish I might'
+
+
+def make_baselist(sentence):
+    """Create baselist from an inputted sentence."""
+    baselist = sentence.split()
+    return baselist
